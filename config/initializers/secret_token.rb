55ca7d316e908e2789a64a6734ecf638cb019a7f
JRP <jamesroypoulter@gmail.com>
2013-07-10 16:22:25 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feedbuzzv1::Application.config.secret_token = '988dcf3c7157d56a19bbb6525c99d5d088cb3efb3655c0dd5e44d4551b60b7727de6343ba30836f6242f077a0442b85d12b781553047ca3df9eac33996cb47fa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Feedbuzzv1::Application.config.session_store :cookie_store, key: '_feedbuzzv1_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Feedbuzzv1::Application.config.session_store :active_record_store
